4385b531d18e0a9586a9e2f6d73a6ba0870a692d
Osama Elsonbaty <sonbatyosama@gmail.com>
2026-03-18 03:06:17 +0200

    Setup alembic database url and metadata for auto-generation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 
-from app.core.db import Base, engine
 from app.api.routes import login, users, urls
 
 app = FastAPI()
@@ -8,9 +7,3 @@ app.include_router(login.router)
 app.include_router(users.router)
 app.include_router(urls.router)
 app.include_router(urls.redirect_router)
-
-
-# TODO: How do we handle this professionally
-@app.on_event("startup")
-def on_startup():
-    Base.metadata.create_all(bind=engine)
